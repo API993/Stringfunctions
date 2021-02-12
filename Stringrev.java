f33c3a881372a6654818a361415fff1e35d4af26
unknown <abhishek.thorgule@apisero.com>
2021-02-12 18:09:33 +0530

    first commit,Program reverses a string

---
+++ Stringrev.java
@@ -0,0 +1,36 @@
+/*
+3. write a java program 
+   take first string iterate the string store those letters in a list
+   reverse the list
+   iterator everything and check if its same as original
+ */  
+
+import java.util.*;
+public class Stringrev 
+{
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+         System.out.println("ENTER STRING");
+         String str = sc.nextLine();
+         
+         ArrayList<Character> arr = new ArrayList<>();
+         
+         for(int i = 0 ;i<str.length();i++)
+        	 arr.add(str.charAt(i));
+        
+     //    System.out.println(arr);
+         Collections.reverse(arr);
+        
+         StringBuilder sb = new StringBuilder();
+         
+         for(int i =0 ; i<arr.size() ; i++)
+        	 sb = sb.append(arr.get(i));
+         
+         if(str.equals(sb.toString()))
+        	 System.out.println("STRING IS A PALINDROME");
+         
+         else
+        	 System.out.println("STRING IS NOT A PALINDROME");
+	}
+}
